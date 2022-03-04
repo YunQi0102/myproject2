bd4a468de9eceff6d89b932607ce11eefe19932e
Yun Qi <mei970073@gmail.com>
2022-03-04 16:14:54 +0800

    Scoring

---
+++ src/main/java/com/fju/Scoring.java
@@ -0,0 +1,10 @@
+package com.fju;
+
+public class Scoring {
+    public static void main(String[] args) {
+        Student jack = new Student();
+        jack.name = "Jack";
+        jack.english = 77;
+        jack.math = 51;
+    }
+}
---
+++ src/main/java/com/fju/Student.java
@@ -0,0 +1,7 @@
+package com.fju;
+
+public class Student {
+    String name;
+    int english;
+    int math;
+}
