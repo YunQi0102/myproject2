f412c8dcfcc89234355edef01115fecc06dc171b
mei970073 <93147911+YunQi0102@users.noreply.github.com>
2022-03-18 16:13:54 +0800

    Add files via upload

--- src/main/java/com/fju/GoldenCustomer.java
+++ src/main/java/com/fju/GoldenCustomer.java
@@ -1,8 +1,9 @@
 package com.fju;
 
-public class SilverCustomer extends Customer {
-    public SilverCustomer(String id, int amount) {
+public class GoldenCustomer extends SilverCustomer {
+    public GoldenCustomer (String id, int amount) {
         super(id, amount);
+        off = 0.2f;
     }
     @Override
     public void print() {
