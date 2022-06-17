30d502d9e2c510d124fb3835032c23e9f181a112
YunQi <mei970073@gmail.com>
2022-06-17 16:35:06 +0800

    BufferedReader

--- src/main/java/com/fju/score/ScoringFile.java
+++ src/main/java/com/fju/score/ScoringFile.java
@@ -9,11 +9,24 @@ public class ScoringFile {
     public static void main(String[] args) {
         try {
             FileReader fileReader = new FileReader("scores.txt");//throws
-            int data = fileReader.read();
-            while (data != -1) {
-                System.out.print((char)data);
-                data = fileReader.read();
+            BufferedReader reader = new BufferedReader(fileReader);
+            //Jack,77,51
+            String line = reader.readLine();
+            while (line != null) {
+//                System.out.println(line);
+                String[] token = line.split(",");
+                String name = token[0];
+                int english = Integer.parseInt(token[1]);
+                int math = Integer.parseInt(token[2]);
+                Student stu = new Student(name, english, math);
+                stu.print();
+                line = reader.readLine();
             }
+//            int data = fileReader.read();
+//            while (data != -1) {
+//                System.out.print((char)data);
+//                data = fileReader.read();
+//            }
         } catch (FileNotFoundException e) {
             System.out.println("檔案讀取失敗");
         } catch (IOException e) {
